0e523d65ca30a5968552c25433a04b7f39e8a2f3
Hope Paasch <hepaasch@gmail.com>
2019-11-24 08:15:09 -0500

    Toying with reversing a string and runes

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/hpaasch/stringutil"
+)
 
 func main() {
-  fmt.Println("Big cows hide here.")
+  cows := "Big cows hide here."
+  fmt.Println("cows:", stringutil.Reverse(cows))
+
+  pigs := "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+  fmt.Println("pigs:", pigs)
+
+  for i:=0; i < len(pigs); i++ {
+    fmt.Printf("%x ", pigs[i])
+  }
+
+  fmt.Println("%x\n", pigs)
+
+  testSlice := "dog is good"
+  for i:=0; i<len(testSlice); i++ {
+    fmt.Println(testSlice[i])
+  }
+
+  for _, letter := range testSlice {
+    fmt.Println("it:", letter)
+  }
+
+  sum := 1
+  for sum < 5 {
+    fmt.Println(sum)
+    sum += 1
+  }
+
+  slice := make([]int, 10)
+  for i, _ := range slice {
+    slice[i] = 1 << uint(i)
+  }
+  fmt.Println(slice)
+
 }
